4ec0fda9edb528abf23f783df65533a6f124223b
Jonathan DeCollibus <jon@superiordigital.design>
2026-06-30 17:43:56 +0200

    Update cask checksum for notarized release

--- Casks/hog.rb
+++ Casks/hog.rb
@@ -1,6 +1,6 @@
 cask "hog" do
   version "0.1.0"
-  sha256 "43e69abfb2eaf0c50ee83b1f73bcef9244d98a49bc9636f056dfdf4fbff2140b"
+  sha256 "7b31cd45f6aa15fd30aae7beb75148fe6bc3cd4a6bbdee771d94d777fbed7c91"
 
   url "https://github.com/svperior-jon/hog/releases/download/v#{version}/Hog-#{version}.zip"
   name "Hog"
